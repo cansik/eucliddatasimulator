6ac276ee66f98ec540df4e3c6ddc546b96423578
Florian Bruggisser <florian@nexpose.ch>
2015-11-10 15:26:02 +0100

    fixed setup packages

--- euclid_stubs_generator/setup.py
+++ euclid_stubs_generator/setup.py
@@ -3,11 +3,10 @@ from distutils.core import setup
 setup(
     name='euclid_stubs_generator',
     version='1.0.0',
-    packages=['test', 'test_data.output', 'test_data.output.bin',
-              'euclid_stubs_generator', 'euclid_stubs_generator.executors'],
+    packages=['euclid_stubs_generator', 'euclid_stubs_generator.executors'],
     url='',
     license='',
     author='Andreas Luescher / Florian Bruggisser',
     author_email='florian.bruggisser@students.fhnw.ch',
-    description=''
+    description='Euclic Test-Stubs Generator'
 )
